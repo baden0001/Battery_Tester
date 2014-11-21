057746d20d5b270cf6b55154757bbfb07f06f3d0
baden0001 <willbaden@hotmail.com>
2014-11-21 06:24:50 -0600

    V0_02 Update
    The Resistor testing program now includes the detection of the supply
    voltage to auto range the analog input.

--- source_code/Arduino_Programs/Resistor_Bank_Testing/Resistor_Bank_Testing.ino
+++ source_code/Arduino_Programs/Resistor_Bank_Testing/Resistor_Bank_Testing.ino
@@ -20,41 +20,66 @@
  12.97                 13.01
  13.51                 13.47
  
- 11/9/14 Test:
- Bank 1:
- 11.56 - 11.55V @ 32.5 amps
- .3553846 ohms
- 11.56 - 11.4V @ 33.1 amps
- 
- Bank 2:
- 11.96 - 11.79 @ 14.6 amps
- .807534 ohms
- 
- Bank 3:
- 12.14 - 11.91 @ 7.8 amps
- 1.526923
- 
  11/10/14 Test
  6.4 amps measured with VOM meter and
  6.6 amps measured with in RobotShop meter
  12.11V reading on voltage divider.
  
+ 11/20/14 Test
+ RobotShop meter appears to be measuring 1.3Amps lower than ammeter.
+ WorkSpace is 38F
+ 
+ 11/9/14 Test:
+ Bank 1:
+ 11.56 - 11.55V @ 32.5 amps
+ .3553846 ohms
+ 11.56 - 11.4V @ 33.1 amps
+ .3444 ohms
+ 11/20/14 Test:
+ 11.04 @ 30 amps
+ .368 ohms (measured with voltmeter and Arduino current meter) 
+ 
+ Bank 2:
+ 11.96 - 11.79 @ 14.6 amps
+ .807534 ohms
+ 11/20/14 Test:
+ 11.24 @ 12.4 amps 
+ .906 ohms (measured with voltmeter and Arduino current meter)
+ 
+ Bank 3:
+ 12.14 - 11.91 @ 7.8 amps
+ 1.526923
+ 11/20/14 Test:
+ 11.4V @ 6.36 amps (using meters not Arduino)
+ 1.79 ohms
  
  Bank 4:
  12.22 - 11.98 @ 5.0 amps
  2.396 ohms
+ 11/20/14 Test:
+ 11.44V @ 4.26 amps (using meters not Arduino)
+ 2.685 ohms
  
  Bank 5:
  12.41 - 12.14 @ 1.5 amps
  8.09333 ohms
+ 11/20/14 Test:
+ 11.65V @ 1.15amps (using meters not Arduino)
+ 10.13 ohms (measured 9.8 ohms)
  
  Bank 6:
  12.43 - 12.15 @ 1.5 amps
  8.1 ohms
+ 11/20/14 Test:
+ 11.66V @ 1.15amps (using meters not Arduino)
+ 10.13 ohms (measured 9.8 ohms)
  
  Bank 7:
  12.41 - 12.14 @ 1.4 amps
  8.67 ohms
+ 11/20/14 Test:
+ 11.65V @ 1.09 amps (using meters not Arduino)
+ 10.688 ohms (measured 10.5 ohms)
  
  Inputs monitored:
  Current Sensor        Analog input 0  0-5V
@@ -79,6 +104,9 @@
  Revisions:
  
  V0.01      Initial release.
+ V0.02      Updated to include 1.1V reference to calculate 
+             actual supplied voltage.  This can then measure
+             more accurate voltage from the 12V battery
  
  */
 #define CurrentSensor 0    //Current sensor input
@@ -96,11 +124,12 @@ int TestState = 0;  //Create state machine, set to wait for key press
                     // 1 = Send out 'Send Start to begin'
                     // 2 = waiting for 'Start' command to start test
                     // 3 = Battery test in progress
-int BatteryVoltage = 0;  //Store battery voltage
-int BatteryCurrent = 0;  //Store battery current
+long BatteryVoltage = 0;  //Store battery voltage
+long BatteryCurrent = 0;  //Store battery current
 String ResistorBank = "";  //Keep track of resistor banks
 char buffer[15];  //buffer to store data from serial port, array size of 15
 int SamplePeriod = 5000;  //Period of time between voltage checks
+long ActualVoltage;
 
 void setup() {
   // initialize serial communication:
@@ -124,11 +153,11 @@ void loop() {
     SendStatus();
     digitalWrite(HalfOhm, LOW);
     digitalWrite(OneOhm, LOW);
-    digitalWrite(TwoOhm, HIGH);
+    digitalWrite(TwoOhm, LOW);
     digitalWrite(ThreeOhm, LOW);
     digitalWrite(TenOhm1, LOW);
     digitalWrite(TenOhm2, LOW);
-    digitalWrite(TenOhm3, LOW);
+    digitalWrite(TenOhm3, HIGH);
     delay(SamplePeriod);  //Delay for sampleperiod     
   
 }
@@ -156,16 +185,54 @@ void SendEndStatus() {
 }  
 
 void ReadBatteryStatus() {
+//Calculate the shift that can be applied to the current and voltage 
+  // analog inputs
+  // 1023 bits in entire range of analog input
+  // 5V/1023 = .00489 volts/bit
+  // 5.15V/1023 = .00503 volts/bit
+  // 4.85V/1023 = .00474 volts/bit
+  ActualVoltage = readVcc();
   //Read battery voltage.  Need to rescale this according to actual sensor used
+  //This scaling appears to be ok except when the test is under a load
+  //  The voltage appears to stay higher by another .15V
   BatteryVoltage = analogRead(VoltageSensor);
-  BatteryVoltage = map(BatteryVoltage, 0, 1023, 0, 2725);
-    
+  //remap to have the actual voltage of the analog input
+  BatteryVoltage = map(BatteryVoltage, 0, 1023, 0, ActualVoltage);
+  //remap 0-5V range to calculated voltage divider range.
+  BatteryVoltage = map(BatteryVoltage, 0, 5000, 0, 2725);
+   
   //Read in Current.  Need to rescale this according to actual sensor used
+  //Up to 6.5 Amps the current sensed on the RobotShop was consistent
+  //  with an ammeter
   BatteryCurrent = analogRead(CurrentSensor);
-  //BatteryCurrent = (BatteryCurrent-510)*5/1024/.04-.04 ; white paper calculation
-  BatteryCurrent = map(BatteryCurrent, 101, 922, -500, 500); //Full scale remapping(0,1023,-625,625)
-                                                             //Sensor full scale remapping(101, 922, -500, 500)  
+  //remap to have the actual voltage of the analog input
+  BatteryCurrent = map(BatteryCurrent, 0, 1023, 0, ActualVoltage);
+  
+  Serial.println("Actual Voltage = " + String(BatteryCurrent));
+  //remap to current values
+  // 2.5V + 50*.04 = 4.5V
+  // 2.5V - 50*.04 = .5V
+  BatteryCurrent = map(BatteryCurrent, 500, 4500, -500, 500);
 }
+
+//This returns the calculated Vcc voltage that is supplied to the UNO
+// and should not affect the reference when a pin is checked
+// via analogRead() due to the Arduino software writing the 
+// requested reference (default in this case).
+// Following Code from Tinkerit:
+long readVcc() {
+  long result;
+  // Read 1.1V reference against AVcc
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA,ADSC));
+  result = ADCL;
+  result |= ADCH<<8;
+  result = 1126400L / result; // Back-calculate AVcc in mV
+  return result;
+}
+// End Tinkerit code
 
 
 
